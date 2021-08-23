92257060e3ef5ca3c257c5e927880f5fd6adcf33
Pedro Selvate <pedroselvate72@gmail.com>
2021-08-23 10:03:38 -0300

    Adding input robot and a save-content robot

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-//Readline vai ser útil para pegar o input do usuário via terminal
-const readline = require('readline-sync')
 
 const robots = {
-   text: require('./robots/text.js')
+   input: require('./robots/inputs.js'),
+   text: require('./robots/text.js'),
+   state: require('./robots/save-content.js')
 }
 
 //Esta função vai agrupar tudo
 async function start(){
-   //Objeto que armazena tudo que for encontardo nas buscas
-   const content = {
-      maximumSentences: 7
-   }
 
-   //Vai criar  atributos deste objeto e adicionar o valor que as funções retornarem
-   content.searchTerm = askAndReturnSearchTerm()
-   content.prefix = askAndReturnPrefix()
-   content.lang = askAndReturnLang()
-
+   robots.input()
    //Passando o conteudo para o robô de texto
-   await robots.text(content)
+   await robots.text()
 
-   function askAndReturnSearchTerm(){
-      //Este metodo question vai injetar no terminal uma pergunta
-      return readline.question('Type a word for searching: ')
-   }
-
-   function askAndReturnPrefix(){
-      const prefixes = ['Who is', 'What is', 'The History of']
-      const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Select one option: ')
-      const selectedPrefixText = prefixes[selectedPrefixIndex];
-      return selectedPrefixText
-   }
-
-   function askAndReturnLang(){
-      const lang = ['pt', 'en']
-      const selectedLangIndex = readline.keyInSelect(lang, 'Select a language: ')
-      const selectedLangText = lang[selectedLangIndex]
-      return selectedLangText
-   }
-
-   console.log(JSON.stringify(content, null, 4))
-
+   const content = robots.state.load()
+   console.dir(content, { depth: null })
 }
 start()
